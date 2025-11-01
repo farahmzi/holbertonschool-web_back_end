a2ce417e4adf358e5be91efb898c3d3c5512e7c2
farahmzi <farahalmzini@gmail.com>
2025-11-01 18:37:20 +0000

    Initialize project setup with Node, Babel, Jest, and ESLint configuration

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = { env: { browser: false, es2021: true, node: true, jest: true }, extends: ['eslint:recommended'], parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }, rules: {} };
